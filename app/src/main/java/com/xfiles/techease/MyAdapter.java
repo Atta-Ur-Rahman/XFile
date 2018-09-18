c5fac2c71778ba4d1c49f43b9845f2a35118cfc5
Zahid Khan <k.zahid@leadergroup.com>
2018-09-18 17:14:27 +0300

    added code for speech recogination but not working

--- app/src/main/java/com/xfiles/techease/MyAdapter.java
+++ app/src/main/java/com/xfiles/techease/MyAdapter.java
@@ -2,20 +2,29 @@ package com.xfiles.techease;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechSettings;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
 import br.com.joinersa.oooalertdialog.Animation;
 import br.com.joinersa.oooalertdialog.OnClickListener;
 import br.com.joinersa.oooalertdialog.OoOAlertDialog;
@@ -70,15 +79,22 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             }
         });
 
-      holder.ivText.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
+        holder.ivText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-              strPath = "/storage/emulated/0/XFiles/" + fileHelper.getStrRecordingFilePath();
-              Utilities.putValueInEditor(context).putString("record_path",strPath).commit();
-              Utilities.connectFragment(context,new TranscriberFragment());
-          }
-      });
+                strPath = "/storage/emulated/0/XFiles/" + fileHelper.getStrRecordingFilePath();
+                Utilities.putValueInEditor(context).putString("record_path", strPath).commit();
+                Utilities.connectFragment(context, new TranscriberFragment());
+            }
+        });
+
+        holder.btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TestSpeech().execute();
+            }
+        });
 
         holder.ivDelete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,7 +135,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         TextView tvRecordingFiles, tvRecordingDuration, tvRecordingDateTime;
         LinearLayout linearLayout;
-        ImageView ivDelete,ivText;
+        ImageView ivDelete, ivText;
+        Button btnConvert;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -129,6 +146,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             tvRecordingFiles = itemView.findViewById(R.id.tv_recording_file);
             tvRecordingDuration = itemView.findViewById(R.id.tv_recording_duration);
             tvRecordingDateTime = itemView.findViewById(R.id.tv_recording_date_time);
+            btnConvert = itemView.findViewById(R.id.btn_convert);
             ivDelete = itemView.findViewById(R.id.iv_delete);
             ivText = itemView.findViewById(R.id.iv_text);
 
@@ -153,4 +171,43 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         Utilities.connectFragmentWithOutBackStack(context, new RecordingViewFragment());
     }
+
+    public class TestSpeech extends AsyncTask<Void, Void, RecognizeResponse> {
+        RecognizeResponse response;
+
+        @Override
+        protected RecognizeResponse doInBackground(Void... voids) {
+
+            try (SpeechClient speechClient = SpeechClient.create(SpeechSettings.newBuilder().setCredentialsProvider(null).build())) {
+                RecognitionConfig.AudioEncoding encoding = RecognitionConfig.AudioEncoding.FLAC;
+                int sampleRateHertz = 44100;
+                String languageCode = "en-US";
+                RecognitionConfig config = RecognitionConfig.newBuilder()
+                        .setEncoding(encoding)
+                        .setSampleRateHertz(sampleRateHertz)
+                        .setLanguageCode(languageCode)
+                        .build();
+                String uri = Uri.parse("android.resource:///com.xfiles.techease/" + R.raw.audio).toString();
+                RecognitionAudio audio = RecognitionAudio.newBuilder()
+                        .setUri(uri)
+                        .build();
+                response = speechClient.recognize(config, audio);
+                Log.d("ads", "Asd");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(RecognizeResponse recognizeResponse) {
+            super.onPostExecute(recognizeResponse);
+            if (recognizeResponse != null) {
+                Log.d("asd", "asd");
+            } else {
+                Log.d("asd", "asd");
+            }
+        }
+    }
+
 }
